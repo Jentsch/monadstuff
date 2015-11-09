1553a3a0b0cdcc5b1ff8b44fe36c505329ae1c96
Jentsch <d.jentsch@fu-berlin.de>
2015-11-09 18:17:57 +0100

    Added documentation

--- src/main/scala/AutoFunctor.scala
+++ src/main/scala/AutoFunctor.scala
@@ -1,12 +1,17 @@
 
 import scala.language.higherKinds
 
-case class AutoFunctor[SI](get: SI) {
+/**
+  * Allows to automagically get the right functor transformer (?) for the given function.
+  * @param data item within the auto functor
+  * @tparam Shape type of data
+  */
+case class AutoFunctor[Shape](data: Shape) {
   /**
    * map : ```A[B[...X]] -> (X -> Y) -> A[B[...Y]]```, ```X``` could be a functor.
-   * 
+    *
    * Requires that the type of ```f``` is explicitly given.
    */
-  def map[I, O, SO](f: I => O)(implicit strategy: AutoFunctorStrategy[SI, I, O]): AutoFunctor[strategy.SO] =
-    new AutoFunctor(strategy.map(get, f))
+  def map[I, O, SO](f: I => O)(implicit strategy: AutoFunctorStrategy[Shape, I, O]): AutoFunctor[strategy.SO] =
+    new AutoFunctor(strategy.map(data, f))
 }
--- src/main/scala/AutoFunctorStrategy.scala
+++ src/main/scala/AutoFunctorStrategy.scala
@@ -3,18 +3,35 @@ import scala.language.higherKinds
 import scalaz.Functor
 
 /**
+  * This traits helps the map Method of the AutoFunctor. Aside testing there
+  * should be no reason to construct this objects manually.
   *
+  * @tparam SI Requested accepting type
   * @tparam I Parameter of the function (in)
   * @tparam O Return type of the function (out)
   */
 trait AutoFunctorStrategy[SI, I, O] {
+  /**
+    * Shape of the return type.
+    */
   type SO
 
   def map(data: SI, f: I => O): SO
 }
 
+
+/**
+  * This traits helps the map Method of the AutoFunctor. Aside testing there
+  * should be no reason to construct this objects manually.
+  */
 object AutoFunctorStrategy {
 
+  /**
+    * End of the recursion.
+    *
+    * @tparam I input type
+    * @tparam O output type
+    */
   implicit def direct[I, O] =
     new AutoFunctorStrategy[I, I, O] {
       type SO = O
@@ -22,12 +39,22 @@ object AutoFunctorStrategy {
       def map(data: I, f: I => O) = f(data)
     }
 
-  implicit def recursive[F[_], ISI, I, O](implicit outer: Functor[F], inner: AutoFunctorStrategy[ISI, I, O]) =
-    new AutoFunctorStrategy[F[ISI], I, O] {
+  /**
+    * One recursion step
+    *
+    * @param outer Functor to unwrap one layer of functors
+    * @param inner Strategy to unwrap the remaining layers
+    * @tparam F type of outer Functor
+    * @tparam IIS inner input shape
+    * @tparam I input type
+    * @tparam O output type
+    */
+  implicit def recursive[F[_], IIS, I, O](implicit outer: Functor[F], inner: AutoFunctorStrategy[IIS, I, O]) =
+    new AutoFunctorStrategy[F[IIS], I, O] {
       override type SO = F[inner.SO]
 
-      override def map(data: F[ISI], f: I => O): SO = {
-        outer.map(data) { (unwrapped: ISI) =>
+      override def map(data: F[IIS], f: I => O): SO = {
+        outer.map(data) { (unwrapped: IIS) =>
           inner.map(unwrapped, f)
         }
       }
