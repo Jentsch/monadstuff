fcdeecc6151466cfe91c5bcbca58396b7d58ee31
Jentsch, Daniel <daniel.jentsch_external@immobilienscout24.de>
2015-11-10 18:27:57 +0100

    Naming

--- src/main/scala/AutoMonad.scala
+++ src/main/scala/AutoMonad.scala
@@ -1,15 +1,17 @@
 
-final case class AutoMonad[Shape](data: Shape) {
+final case class AutoMonad[Shape](get: Shape) {
+
   /**
     * map : ```A[B[...X]] -> (X -> Y) -> A[B[...Y]]```, ```X``` could still be a functor.
     *
     * Requires that the type of ```f``` is explicitly given.
     */
   def map[I, O](f: I => O)(implicit strategy: AutoFunctorStrategy[Shape, I, O]): AutoMonad[strategy.SO] =
-    AutoMonad(strategy.map(data, f))
+    AutoMonad(strategy.map(get, f))
 
   def flatMap[I, O](f: I => O)(implicit strategy: AutoMonadStrategy[Shape, I, O]): AutoMonad[strategy.SO] =
-    AutoMonad(strategy.flatMap(data)(f))
+    AutoMonad(strategy.flatMap(get)(f))
   
-  def get = data
+  def merge[I, O](f: I => O)(implicit strategy: MergeStrategy[Shape, I, O]): AutoMonad[strategy.SO] =
+    AutoMonad(strategy.merge(get)(f))
 }
