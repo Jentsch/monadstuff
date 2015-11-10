2e58e4e09a897ef64b4f487800e9978e3ac9a083
Jentsch, Daniel <daniel.jentsch_external@immobilienscout24.de>
2015-11-10 16:51:54 +0100

    Revert "Added documentation"
    This reverts commit 1553a3a0b0cdcc5b1ff8b44fe36c505329ae1c96.

--- src/main/scala/AutoFunctor.scala
+++ src/main/scala/AutoFunctor.scala
@@ -1,17 +1,12 @@
 
 import scala.language.higherKinds
 
-/**
-  * Allows to automagically get the right functor transformer (?) for the given function.
-  * @param data item within the auto functor
-  * @tparam Shape type of data
-  */
-case class AutoFunctor[Shape](data: Shape) {
+case class AutoFunctor[SI](get: SI) {
   /**
    * map : ```A[B[...X]] -> (X -> Y) -> A[B[...Y]]```, ```X``` could be a functor.
-    *
+   * 
    * Requires that the type of ```f``` is explicitly given.
    */
-  def map[I, O, SO](f: I => O)(implicit strategy: AutoFunctorStrategy[Shape, I, O]): AutoFunctor[strategy.SO] =
-    new AutoFunctor(strategy.map(data, f))
+  def map[I, O, SO](f: I => O)(implicit strategy: AutoFunctorStrategy[SI, I, O]): AutoFunctor[strategy.SO] =
+    new AutoFunctor(strategy.map(get, f))
 }
